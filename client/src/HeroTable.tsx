c52448c2714e19824416777b2b1b8561c5fb320f
Ville Tanttu <vilunen@gmail.com>
2020-11-02 00:06:05 +0200

    Trying out Sass

--- client/src/HeroTable.tsx
+++ client/src/HeroTable.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import heroService, {IHero} from "./heroService";
+import './HeroTable.css'
 
 const HeroTable = () => {
     const [heroes, setHeroes] = useState<IHero[]>([])
@@ -7,7 +8,7 @@ const HeroTable = () => {
     useEffect(() => {
         heroService
             .getHeroes()
-            .then(h => setHeroes(h))
+            .then(heroes => setHeroes(heroes))
     }, [])
 
     return (
@@ -15,36 +16,37 @@ const HeroTable = () => {
             <thead>
                 <tr>
                     <th>Hero</th>
-                    <th>Base health</th>
-                    <th>Base health regen</th>
-                    <th>Base mana</th>
-                    <th>Base mana regen</th>
-                    <th>Base armor</th>
-                    <th>Base mr</th>
-                    <th>Damage min</th>
-                    <th>Damage max</th>
-                    <th>Base str</th>
-                    <th>Base agi</th>
-                    <th>Base int</th>
-                    <th>Str gain</th>
-                    <th>Agi gain</th>
-                    <th>Attack range</th>
-                    <th>Projectile speed</th>
-                    <th>Attack rate</th>
-                    <th>Movement speed</th>
-                    <th>Turn rate</th>
+                    <th>Attr</th>
+                    <th>HP</th>
+                    <th>HP/s</th>
+                    <th>MP</th>
+                    <th>MP/s</th>
+                    <th>Armor</th>
+                    <th>Dmg (min)</th>
+                    <th>Dmg (max)</th>
+                    <th>Str</th>
+                    <th>Agi</th>
+                    <th>Int</th>
+                    <th>Str+</th>
+                    <th>Agi+</th>
+                    <th>Int+</th>
+                    <th>AR</th>
+                    <th>PS</th>
+                    <th>BAT</th>
+                    <th>MS</th>
+                    <th>TR</th>
                 </tr>
             </thead>
             <tbody>
                 {heroes.map((hero, index) =>
                     <tr key={index}>
                         <td>{hero.localized_name}</td>
+                        <td>{hero.primary_attr}</td>
                         <td>{hero.base_health}</td>
                         <td>{hero.base_health_regen}</td>
                         <td>{hero.base_mana}</td>
                         <td>{hero.base_mana_regen}</td>
                         <td>{hero.base_armor}</td>
-                        <td>{hero.base_mr}</td>
                         <td>{hero.base_attack_min}</td>
                         <td>{hero.base_attack_max}</td>
                         <td>{hero.base_str}</td>
