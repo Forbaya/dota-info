321e9cbba2bf61b00062aacd19ebb0795a99d0b5
Ville Tanttu <vilunen@gmail.com>
2020-11-01 19:53:28 +0200

    Added Cors

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,14 @@
 mod hero;
 mod open_dota_api;
 
+use actix_cors::Cors;
 use actix_web::{get, App, HttpServer};
 use actix_web::web::Json;
 use std::io::Error;
 use crate::hero::Hero;
 use crate::open_dota_api::get_heroes;
 
-#[get("/heroes")]
+#[get("/api/heroes")]
 async fn heroes() -> Result<Json<Vec<Hero>>, Error> {
     let mut heroes = get_heroes()
         .await
@@ -23,8 +24,16 @@ async fn heroes() -> Result<Json<Vec<Hero>>, Error> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(heroes))
-        .bind("127.0.0.1:8080")?
+    HttpServer::new(|| {
+        let cors = Cors::permissive();
+
+        App::new()
+            .wrap(cors)
+            .service(heroes)
+    })
+    .bind("127.0.0.1:8080")?
         .run()
-        .await
+        .await;
+
+    Ok(())
 }
